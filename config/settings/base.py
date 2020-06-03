ce68d01a8ed6d6fb31bd5fdcc5a8c37ccb4ee34b
Karpets Pavel <karpets.pavel@gmail.com>
2020-06-03 23:46:24 +0600

    setup project

---
+++ config/settings/base.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+
+import environ
+
+ROOT_PATH = Path().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(str(ROOT_PATH / '.env'))
+
+SECRET_KEY = env.str('CREDITOR_DJANGO_SECRET_KEY')
+
+DEBUG = env.bool('CREDITOR_DJANGO_DEBUG', default=False)
+
+ALLOWED_HOSTS = env.list('CREDITOR_DJANGO_ALLOWED_HOSTS', default=[])
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [str(ROOT_PATH / 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+DATABASES = {
+    'default': env.db('CREDITOR_DJANGO_DB_URL')
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Asia/Almaty'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = env.str('CREDITOR_DJANGO_STATIC_ROOT', str(ROOT_PATH / 'static_root'))
+STATICFILES_DIRS = [str(ROOT_PATH / 'static')]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
